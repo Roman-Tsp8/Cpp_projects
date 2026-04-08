af0589fc0095df3ebd1bc781d06b36848ed70708
Tom_5975 <romalihenko26@users.noreply.github.com>
2026-04-08 20:17:10 +0300

    implement storage.cpp

---
+++ lab3/storage.cpp
@@ -0,0 +1,19 @@
+#include "storage.h"
+#include <fstream>
+
+using namespace std;
+
+void SaveProducts(const vector<shared_ptr<Product>>& products) {
+    ofstream file("products.txt");
+    for (auto& p : products) {
+        file << p->getPrice() << endl;
+    }
+}
+
+void loadProducts(vector<shared_ptr<Product>>& products) {
+    ifstream file("products.txt");
+    double price;
+    while (file >> price) {
+        products.push_back(make_shared<Product>("Loaded Product", price, 1));
+    }
+}
